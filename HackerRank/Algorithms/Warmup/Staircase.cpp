35a3717cda301244d6ade1a7fadb04c23f9c3e0f
Seokjin Lee <seokjin.dev@gmail.com>
2019-04-08 22:40:46 +0900

    Create Staircase.cpp

---
+++ HackerRank/Algorithms/Warmup/Staircase.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/staircase/problem
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i=0; i<n; ++i) {
+        for (int j=i+1; j<n; ++j)
+            cout << ' ';
+        for (int j=0; j<=i; ++j)
+            cout << '#';
+        cout << '\n';
+    }
+}
