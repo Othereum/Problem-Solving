d6a4be681523d4a0a974248b14febce821d57e3e
Seokjin Lee <seokjin.dev@gmail.com>
2019-04-23 18:11:16 +0900

    Create 1463 - 1로 만들기.cpp

---
+++ BOJ/1463 - 1로 만들기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1'000'001];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i{ 2 }; i <= n; ++i)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+		if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+	}
+	cout << dp[n];
+}
