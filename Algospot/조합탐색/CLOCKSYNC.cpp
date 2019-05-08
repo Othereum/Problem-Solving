8ff75ecfd66078a1f3296286c208039547d97ced
Seokjin Lee <seokjin.dev@gmail.com>
2019-05-09 07:54:36 +0900

    Update CLOCKSYNC.cpp

--- Algospot/조합탐색/CLOCKSYNC.cpp
+++ Algospot/조합탐색/CLOCKSYNC.cpp
@@ -20,16 +20,23 @@ void ClockSync(ClockArr& Clocks, int& OutMinCnt, const int SwitchIdx, const int 
 {
 	if (SwitchIdx < SwitchesNum)
 	{
+		// 스위치 한 번도 안누름
 		ClockSync(Clocks, OutMinCnt, SwitchIdx + 1, SwitchPressCnt);
+
+		// 스위치를 3번 누름
 		for (auto i = 1; i <= 3; ++i)
 		{
+			// 누르기
 			for (auto ClockIdx : Switches[SwitchIdx])
 			{
 				Clocks[ClockIdx] += 3;
 				if (Clocks[ClockIdx] > 12) Clocks[ClockIdx] = 3;
 			}
+			// 한 번씩 누를 때마다 나머지 스위치들에 대해 조합 탐색
 			ClockSync(Clocks, OutMinCnt, SwitchIdx + 1, SwitchPressCnt + i);
 		}
+
+		// 3번 눌렀던 스위치 복구
 		for (auto i = 1; i <= 3; ++i)
 		{
 			for (auto ClockIdx : Switches[SwitchIdx])
@@ -39,10 +46,11 @@ void ClockSync(ClockArr& Clocks, int& OutMinCnt, const int SwitchIdx, const int 
 			}
 		}
 	}
-	else
-		if (SwitchPressCnt < OutMinCnt)
-			if (all_of(Clocks.begin(), Clocks.end(), [](int i) {return i == 12; }))
-				OutMinCnt = SwitchPressCnt;
+	// 기저사례: 모든 스위치를 탐색함 (하나의 '스위치 누르기 조합' 완성)
+	else 
+		// 모든 시계가 12시라면 현재 조합의 스위치 누른 횟수와 최소값을 비교하여 저장
+		if (SwitchPressCnt < OutMinCnt && all_of(Clocks.begin(), Clocks.end(), [](int i) {return i == 12; }))
+			OutMinCnt = SwitchPressCnt;
 }
 
 int ClockSync(ClockArr& Clocks)
