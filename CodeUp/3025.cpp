327e70c189c8f8fffce9fd04ce896068e0b1b8b3
이석진 <seokjin.dev@gmail.com>
2019-05-28 20:09:09 +0900

    Create 3025.cpp

---
+++ CodeUp/3025.cpp
@@ -0,0 +1,32 @@
+// http://codeup.kr/problem.php?id=3025
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 기본적으로 DP 문제
+// C[i]는 x^(i+1) 항의 계수를 나타냄
+// x 부터 시작해 (x+r)을 하나씩 곱해간다
+vector<long long> PolynomialExpansion(int n)
+{
+	vector<long long> C(n);
+	C[0] = 1;
+	for (int r = 1; r < n; ++r)
+	{
+		C[r] = 1;
+		for (int i = r - 1; i > 0; --i)
+			C[i] = C[i - 1] + C[i] * r;
+		C[0] *= r;
+	}
+	return C;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<long long> P = PolynomialExpansion(n);
+	for (int i = P.size() - 1; i > 0; --i)
+		cout << P[i] << "x^" << i + 1 << '+';
+	cout << P[0] << "x^1";
+}
