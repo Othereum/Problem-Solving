3fa4990c98c6b2d1681da3cfcb38be75defb0182
이석진 <seokjin.dev@gmail.com>
2019-05-28 10:46:15 +0900

    Create cut-the-sticks.cpp

---
+++ HackerRank/Algorithms/Implementation/cut-the-sticks.cpp
@@ -0,0 +1,18 @@
+vector<int> cutTheSticks(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    auto a{arr[0]}, cnt{0};
+    auto left{arr.size()};
+    vector<int> ret;
+    for (auto x : arr) {
+        if (a == x)
+            ++cnt;
+        else {
+            ret.push_back(left);
+            left -= cnt;
+            cnt = 1;
+            a = x;
+        }
+    }
+    ret.push_back(cnt);
+    return ret;
+}
