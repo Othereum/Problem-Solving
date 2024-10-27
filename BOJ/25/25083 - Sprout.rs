f22d00c9fa9ec66bde188cf483d999bbc9c9097d
Seokjin Lee <seokjin.dev@gmail.com>
2024-10-27 15:06:41 +0900

    25083 - Sprout

---
+++ BOJ/25/25083 - Sprout.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!(r#"         ,r'"7"#);
+    println!(r#"r`-_   ,'  ,/"#);
+    println!(r#" \. ". L_r'"#);
+    println!(r#"   `~\/"#);
+    println!(r#"      |"#);
+    println!(r#"      |"#);
+}
