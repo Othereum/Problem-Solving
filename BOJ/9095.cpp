faea3abd570482aaf9fbf7ae6afc5e56e66ab02f
Seokjin Lee <seokjin.dev@gmail.com>
2025-03-27 13:54:03 +0900

    Update 9095.cpp

--- BOJ/9095.cpp
+++ BOJ/9095.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int CACHE[10];
 int solve(int n)
 {
     if (n < 0) return 0;
     if (n <= 1) return 1;
-    return solve(n-1) + solve(n-2) + solve(n-3);
+
+    int& ret = CACHE[n-1];
+    if (ret != -1) return ret;
+    
+    return ret = solve(n-1) + solve(n-2) + solve(n-3);
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    memset(CACHE, -1, sizeof(CACHE));
 
     int numTestCase;
     cin >> numTestCase;
