f141a13d6098e86bf206e76cf76ddbdda22b8122
Seokjin Lee <seokjin.dev@gmail.com>
2025-06-21 21:54:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[1025][1025];
+
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
@@ -8,28 +10,16 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> vec(N);
-    for (vector<int>& v : vec) {
-        v.resize(N);
-        cin >> v[0];
-        for (int i = 1; i < N; i++) {
-            cin >> v[i];
-            v[i] += v[i-1];
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
+            arr[i][j] += arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
         }
     }
 
     for (int m = 0; m < M; m++) {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        
-        int sum = 0;
-        for (int row = x1-1; row < x2; row++) {
-            int s = vec[row][y2-1];
-            if (y1 >= 2) {
-                s -= vec[row][y1-2];
-            }
-            sum += s;
-        }
-        cout << sum << '\n';
+        cout << (arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1]) << '\n';
     }
 }
